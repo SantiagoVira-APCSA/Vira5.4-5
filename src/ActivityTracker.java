30d01ce5ac8b9751c10ee45c25a1cdb37756c906
SantiagoVira <santiagovira06@gmail.com>
2022-12-09 12:03:15 -0500

    Code

--- src/ActivityTracker.java
+++ src/ActivityTracker.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class ActivityTracker {
+public class ActivityTracker {
+    private int targetGoal;
+    private int totalMinutes;
+    private int sessions;
+    private int longestSession;
+
+    public ActivityTracker(int goal) {
+        targetGoal = goal;
+    }
+
+    public void logMinutes(int amount) {
+        totalMinutes += amount;
+        sessions++;
+        if (amount > longestSession) longestSession = amount;
+    }
+
+    public int getSessions() {
+        return sessions;
+    }
+
+    public int getTotalMinutes() {
+        return totalMinutes;
+    }
+
+    public int getMaxMinutes() {
+        return longestSession;
+    }
+
+    public boolean goalAchieved() {
+        return totalMinutes >= targetGoal;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your goal in minutes: ");
+        int goal = scan.nextInt();
+
+        // create an ActivityTracker object
+        ActivityTracker tracker = new ActivityTracker(goal);
+
+        // repeatedly ask user to enter an activity session's number of minutes
+        // and enter -1 when done entering
+        int newSession = 1;
+        while (newSession > 0) {
+            System.out.print("Enter the number of minutes for your session or -1 to stop: ");
+            newSession = scan.nextInt();
+            if (newSession > 0) {
+                tracker.logMinutes(newSession);
+            }
+        }
+
+        // log the number of minutes associated with each activity session
+
+        // when the user is done entering numbers, end the loop and
+        // tell the user their stats:
+        System.out.println("Number of sessions: " + tracker.getSessions());
+        System.out.println("Total number of minutes: " + tracker.getTotalMinutes());
+        System.out.println("Longest session: " + tracker.getMaxMinutes());
+        System.out.println("You have" + (tracker.goalAchieved() ? "" : " not") + " achieved your goal");
+        // - total sessions logged
+        // - total minutes logged
+        // - max minutes logged
+        // - did they reach their goal?
+
     }
 }
